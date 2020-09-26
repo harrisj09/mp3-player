ddc505ef1c7f501fb073d4060c8f0769d858bce6
John Harris <vosm803@gmail.com>
2020-09-26 14:25:33 -0400

    Added gui layout

---
+++ src/main/java/logic/MusicNode.java
@@ -0,0 +1,13 @@
+package logic;
+
+import java.io.BufferedReader;
+
+public class MusicNode {
+    private BufferedReader reader;
+    private String path;
+
+    public MusicNode(String path) {
+        this.path = path;
+        //reader = new BufferedReader();
+    }
+}
